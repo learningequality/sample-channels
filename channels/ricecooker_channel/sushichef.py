9afbedd6fdfb6a5cc070007dbcc0cab5cecf098b
Ivan Savov <ivan.savov@gmail.com>
2018-05-02 13:43:59 -0400

    manually force-flush stdout buffers

--- channels/ricecooker_channel/sushichef.py
+++ channels/ricecooker_channel/sushichef.py
@@ -43,13 +43,13 @@ class SampleChef(SushiChef):
         """
         channel = self.get_channel(*args, **kwargs)   # create ChannelNode from data in self.channel_info
 
-        print('An ordinary print 1')
+        print('An ordinary print 1', flush=True)
         LOGGER.debug('A testing debug message should not be printed')
         LOGGER.info('Basic INFO message -- should be printed')
         LOGGER.warning('A WARN level message')
         LOGGER.error('An ERROR level message')
         LOGGER.critical('A CRITICAL level message')
-        print('An ordinary print 2')
+        print('An ordinary print 2', flush=True)
 
         self.create_content_nodes(channel)
         self.create_exercise_nodes(channel)
